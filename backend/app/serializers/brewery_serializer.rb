f937a961ab0e0d2aab1f5cd76c6c3df0c594a481
Ariel_Ibarra <arielibarra39@gmail.com>
2021-01-05 19:57:02 -0800

    add new params to b serializer

--- backend/app/serializers/brewery_serializer.rb
+++ backend/app/serializers/brewery_serializer.rb
@@ -1,4 +1,4 @@
 class BrewerySerializer < ActiveModel::Serializer
-  attributes :id, :name, :location
+  attributes :id, :name, :location, :description, :outdoor_dining, :animal_friendly
   has_many :items
 end
